16b5e1ca066e63198212ccfdbe6a82e04a184c79
Alikhan Nurkhat <alikhannurkhat@rb2026-0025.roboauto.lan>
2026-09-04 17:37:35 +0200

    Cleared CI.yml

--- tests/funs_test.cpp
+++ tests/funs_test.cpp
@@ -2,8 +2,9 @@
 #include <string>
 
 int main(int argc, char *argv[]) {
-	int inNum = 0;
-	if (argc > 1) inNum = std::stoi(argv[1]);
+	if (argc == 1) return 1;
+
+	int inNum = std::stoi(argv[1]);
 	bool even = (inNum % 2) == 0;
 	bool isPassed = even == isEven(inNum);
 	return (isPassed) ? 0 : 1;
