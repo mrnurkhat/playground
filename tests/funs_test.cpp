4184fce3694f44f58340a311a45a1091836aadf3
Alikhan Nurkhat <alikhannurkhat@rb2026-0025.roboauto.lan>
2026-09-04 16:28:52 +0200

    Fix CI test

--- tests/funs_test.cpp
+++ tests/funs_test.cpp
@@ -2,6 +2,6 @@
 
 int main() {
 	bool isCorrect = false;
-	isCorrect = isEven(6) && isEven(3) && isEven(0) && isEven(-2) && !isEven(-3);
+	isCorrect = isEven(6) && !isEven(3) && isEven(0) && isEven(-2) && !isEven(-3);
 	return (isCorrect) ? 0 : 1;
 }
