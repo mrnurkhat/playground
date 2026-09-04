4b3e32b78d3d63e0e2d7fdc9a716abbb4f680a67
Alikhan Nurkhat <alikhannurkhat@rb2026-0025.roboauto.lan>
2026-09-04 17:28:35 +0200

    Separate tests fixed second time

--- tests/funs_test.cpp
+++ tests/funs_test.cpp
@@ -4,6 +4,7 @@
 int main(int argc, char *argv[]) {
 	int inNum = 0;
 	if (argc > 1) inNum = std::stoi(argv[1]);
-	bool isPassed = !(inNum % 2) == isEven(inNum);
+	bool even = (inNum % 2) == 0;
+	bool isPassed = even == isEven(inNum);
 	return (isPassed) ? 0 : 1;
 }
